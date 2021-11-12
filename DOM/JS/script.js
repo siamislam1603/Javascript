0e517aeb0afde51b34cacc4ee5ef838d893456ae
Your Name <you@example.com>
2021-11-12 18:31:12 +0600

    Explored DOM accessing elements of a tag name & accessing a specific element by id

---
+++ DOM/JS/script.js
@@ -0,0 +1,7 @@
+console.log(document);
+console.log(document.body);
+console.log('All of the elements of this particular tag in the document:',document.getElementsByTagName('p'));
+console.log('Only a specific element of this id:',document.getElementById('first-paragraph'));
+firstParagraph=document.getElementById('first-paragraph');
+firstParagraph.style.color='green';
+firstParagraph.style.fontSize='30px';
